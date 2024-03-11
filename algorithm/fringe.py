7b90c384f131a609be0bcff711df0c92933c707b
lhgiang040504 <lhgiang040504@gmail.com>
2024-03-11 10:42:54 +0700

    90% fringe

--- algorithm/fringe.py
+++ algorithm/fringe.py
@@ -1,32 +1,10 @@
-"""
-height = 5
-width = 4
-matrix = [[1, 2, 3, 4],
-       [5, 6, 7, 8],
-       [9, 10, 11, 12],
-       [13, 14, 15, 16],
-       [17, 18, 19, 20]]
-
-height = 8
-width = 1
-matrix = [[238],
-       [239],
-       [240],
-       [241],
-       [242],
-       [243],
-       [244],
-       [245]]
-"""
-height, width = map(int, input().split())
-matrix = []
-for _ in range(height):
-    row = list(map(int, input().split()))
-    matrix.append(row)
+import sys
+import math
 
 def find_fringe(height, width):
     parent_lst = []
-    for edge_idx in range(round(min(height, width)/2) if min(height, width) != 1 else 1):
+    num_fringe = math.ceil(min(height, width)/2)
+    for edge_idx in range(num_fringe):
         child_lst = []
         x = edge_idx
         y = edge_idx
@@ -44,9 +22,12 @@ def find_fringe(height, width):
                 break
         parent_lst.append(child_lst)
     return parent_lst
+        
+def solve(height, width, matrix):
 
-def solve(matrix, addresses, height, width):
+    addresses = find_fringe(height, width)
     new_matrix = [[0 for _ in range(width)] for _ in range(height)]
+
     for idx, address in enumerate(addresses):
         if idx % 2 == 0:
             new_address = address[-1:] + address[:-1]
@@ -56,9 +37,16 @@ def solve(matrix, addresses, height, width):
         for new, old in zip(new_address, address):
             new_matrix[old[0]][old[1]] = matrix[new[0]][new[1]]
 
-    return new_matrix
+    for row in new_matrix:
+        sys.stdout.write(' '.join(str(x) for x in row))
+        sys.stdout.write('\n')
 
-addresses = find_fringe(height, width)
-new_matrix = solve(matrix, addresses, height, width)
-for row in new_matrix:
-    print(row)
+height, width = map(int, sys.stdin.readline().split())
+matrix = []
+for _ in range(height):
+    row = list(map(str.strip, sys.stdin.readline().split()))
+    matrix.append(row)
+if height == 1 and width == 1:
+    sys.stdout.write(str(matrix[0][0]))
+else:
+    solve(height, width, matrix)
