cedcd914ee1ae4ddb2e5371801b037dd3ddf3098
lhgiang040504 <lhgiang040504@gmail.com>
2024-03-17 19:21:57 +0700

    loser

---
+++ algorithm/padding.py
@@ -0,0 +1,24 @@
+import sys
+
+def padding(matrix, half_height=None):
+    if half_height is None:
+        max_lengths = [len(str(max(col, key=len))) for col in zip(*matrix)]
+    else:
+        max_lengths = [len(str(max(col, key=len))) for col in zip(*matrix[:half_height])]
+    for row_idx, row in enumerate(matrix):
+        matrix[row_idx] = [(" " * (max_length - len(item))) + item
+                          for item, max_length in zip(row, max_lengths)]
+    return matrix
+
+height, width = map(int, sys.stdin.readline().split())
+matrix = []
+for _ in range(height):
+    row = list(map(lambda num: str(int(num)), sys.stdin.readline().split()))
+    matrix.append(row)
+if height * width < 1000000:
+    matrix = padding(matrix)
+else:
+     matrix = padding(matrix, height // 30)
+for row in matrix:
+    sys.stdout.write(' '.join(x for x in row))
+    sys.stdout.write('\n')
