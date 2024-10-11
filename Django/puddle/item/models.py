f852cb136e2e5e918432d5edc44a73289955de56
lhgiang040504 <lhgiang040504@gmail.com>
2024-10-11 14:12:56 +0700

    practicce django with expert

--- Django/puddle/item/models.py
+++ Django/puddle/item/models.py
@@ -1,17 +1,16 @@
-from django.db import models
 from django.contrib.auth.models import User
+from django.db import models
 
-# Create your models here.
 class Category(models.Model):
     name = models.CharField(max_length=255)
 
     class Meta:
         ordering = ('name',)
         verbose_name_plural = 'Categories'
-
+    
     def __str__(self):
         return self.name
-    
+
 class Item(models.Model):
     category = models.ForeignKey(Category, related_name='items', on_delete=models.CASCADE)
     name = models.CharField(max_length=255)
--- Django/puddle/item/urls.py
+++ Django/puddle/item/urls.py
@@ -1,18 +1,13 @@
-"""
+from django.urls import path
+
+from . import views
+
+app_name = 'item'
+
 urlpatterns = [
-    path('<int:pk>/', views.detail, name='items'),
+    path('', views.items, name='items'),
     path('new/', views.new, name='new'),
     path('<int:pk>/', views.detail, name='detail'),
     path('<int:pk>/delete/', views.delete, name='delete'),
     path('<int:pk>/edit/', views.edit, name='edit'),
-]
-"""
-from django.urls import path, include
-from . import views  # Import views from your products app
-
-app_name = 'item'  # Optional namespace (explained later)
-
-urlpatterns = [
-    path('<int:pk>/', views.detail, name='detail'),
-    # ... other URL patterns for your products app ...
 ]
