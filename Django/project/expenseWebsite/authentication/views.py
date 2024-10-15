8d61947c65f4f0230189cb3d3449a588b0e191b7
lhgiang040504 <lhgiang040504@gmail.com>
2024-10-15 22:21:27 +0700

    setting registor case

--- Django/project/expenseWebsite/authentication/views.py
+++ Django/project/expenseWebsite/authentication/views.py
@@ -4,11 +4,42 @@ import json
 from django.http import JsonResponse
 from django.contrib.auth.models import User
 from validate_email import validate_email
+from django.contrib import messages
 # Create your views here.
 
 class RegistrationView(View):
     def get(self, request):
         return render(request, 'register.html')
+
+    def post(self, request):
+        # GET USER DATA
+        # VALIDATE
+        # create a user account
+
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+
+        context = {
+            'fieldValues': request.POST
+        }
+
+        if not User.objects.filter(username=username).exists():
+            if not User.objects.filter(email=email).exists():
+                if len(password) < 6:
+                    messages.error(request, 'Password too short')
+                    return render(request, 'register.html', context)
+
+                user = User.objects.create_user(username=username, email=email)
+                user.set_password(password)
+                user.is_active = False
+                user.save()
+                
+                messages.success(request, 'Account successfully created')
+                return render(request, 'register.html')
+
+        return render(request, 'register.html')
+
     
 class UsernameValidationView(View):
     def post(self, request):
--- Django/project/expenseWebsite/expenseWebsite/static/js/register.js
+++ Django/project/expenseWebsite/expenseWebsite/static/js/register.js
@@ -1,9 +1,36 @@
 const usernameField = document.querySelector('#usernameField');
 const emailField = document.querySelector('#emailField');
+const passwordField = document.querySelector('#passwordField');
 
 const feedBackArea = document.querySelector('.invalid_feedback');
 const emailFeedBackArea = document.querySelector('.emailFeedBackArea');
+const showPasswordToggle = document.querySelector(".showPasswordToggle")
 
+// USERNAME
+usernameField.addEventListener("keyup", (e) => {
+  const usernameVal = e.target.value;
+
+  usernameField.classList.remove("is-invalid");
+  feedBackArea.style.display = "none";
+
+  if (usernameVal.length > 0) {
+      fetch("/authentication/username_validation", {
+          body: JSON.stringify({ username: usernameVal }),
+          method: "POST",
+      })
+      .then(res => res.json())
+      .then((data) => {     
+        if (data.username_error) {
+            usernameField.classList.add("is-invalid");
+            feedBackArea.style.display = "block";
+            feedBackArea.innerHTML = `<p>${data.username_error}</p>`;
+            submitBtn.disabled = true;
+        }
+      });
+  }
+});
+
+// EMAIL
 emailField.addEventListener("keyup", (e) => {
   const emailVal = e.target.value;
 
@@ -25,27 +52,4 @@ emailField.addEventListener("keyup", (e) => {
         }
       });
   }
-});
-
-usernameField.addEventListener("keyup", (e) => {
-    const usernameVal = e.target.value;
-
-    usernameField.classList.remove("is-invalid");
-    feedBackArea.style.display = "none";
-  
-    if (usernameVal.length > 0) {
-        fetch("/authentication/username_validation", {
-            body: JSON.stringify({ username: usernameVal }),
-            method: "POST",
-        })
-        .then(res => res.json())
-        .then((data) => {     
-          if (data.username_error) {
-              usernameField.classList.add("is-invalid");
-              feedBackArea.style.display = "block";
-              feedBackArea.innerHTML = `<p>${data.username_error}</p>`;
-              submitBtn.disabled = true;
-          }
-        });
-    }
 });
