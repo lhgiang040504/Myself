524e15048044b71cdcb840c43d5e5ce3305f4f9c
lhgiang040504 <lhgiang040504@gmail.com>
2024-10-15 15:11:11 +0700

    fix bug

--- Django/project/expenseWebsite/authentication/urls.py
+++ Django/project/expenseWebsite/authentication/urls.py
@@ -1,6 +1,8 @@
-from .views import RegistrationView
+from .views import RegistrationView, UsernameValidationView
 from django.urls import path
+from django.views.decorators.csrf import csrf_exempt
 
 urlpatterns = [
     path('register', RegistrationView.as_view(), name='register'),
+    path('username_validation', csrf_exempt(UsernameValidationView.as_view()), name='username_validation')
 ]
