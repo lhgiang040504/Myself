c9e84636c4e0767bb621b1bdafaba34c5f8f031a
lhgiang040504 <lhgiang040504@gmail.com>
2024-10-18 16:22:26 +0700

    add data with auth

--- Django/project/expenseWebsite/core/views.py
+++ Django/project/expenseWebsite/core/views.py
@@ -1,12 +1,41 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+from .models import Category, Expense
+from django.contrib import messages
+from django.contrib.auth.models import User
 
 # Create your views here.
 def index(request):
-    if request.user.is_authenticated:
-        return render(request, 'index.html')
-    else:
-        return redirect('login')  # Redirect to login page
+    expenses = Expense.objects.filter(owner=request.user)
+    context = {
+        'expenses': expenses,
+    }
+    return render(request, 'index.html', context)
 
 def add_expense(request):
-    return render(request, 'add_expense.html')
+    categories = Category.objects.all()
+    context = {
+        'categories': categories,
+        'values': request.POST
+    }
+    if request.method == 'GET':
+        return render(request, 'add_expense.html', context)
+
+    if request.method == 'POST':
+        amount = request.POST['amount']
+
+        if not amount:
+            messages.error(request, 'Amount is required')
+            return render(request, 'add_expense.html', context)
+        
+        description = request.POST['description']
+        date = request.POST['expense_date']
+        category = request.POST['category']
+
+        if not description:
+            messages.error(request, 'description is required')
+            return render(request, 'add_expense.html', context)
+
+        Expense.objects.create(owner=request.user, amount=amount, date=date, category=category, description=description)
+        messages.success(request, 'Expense saved successfully')
+        return redirect('index')
