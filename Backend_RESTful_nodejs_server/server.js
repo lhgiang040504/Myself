1e87a54ed021c8e3ed84a56deb2ebf4bea777ab7
lhgiang040504 <lhgiang040504@gmail.com>
2025-01-05 17:11:38 +0700

    views engine

--- Backend_RESTful_nodejs_server/server.js
+++ Backend_RESTful_nodejs_server/server.js
@@ -1,16 +1,23 @@
-// const express = require('express')
-import express from 'express'
+const express = require('express')
+//import express from 'express'
 const app = express()
 const port = 3000
 
-// Route
+// Config template engine
+app.set('views', './src/views')
+app.set('views engine', 'ejs')
+// If error
+// const path from 'path'
+// app.set('views', path.join(__dirname, './src/views'))
+
+// Route 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
 // Route
 app.get('/abc', (req, res) => {
-    res.send('<h1>Check</h1>')
+    res.render('sample.ejs')
 })
 
 app.listen(port, () => {
