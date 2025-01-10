b2856a723dec7865a1ff009026d1e2d7b4fec1fa
lhgiang040504 <lhgiang040504@gmail.com>
2025-01-10 13:41:12 +0700

    update user

--- Backend_RESTful_nodejs_server/src/controllers/homeController.js
+++ Backend_RESTful_nodejs_server/src/controllers/homeController.js
@@ -32,6 +32,42 @@ const getEditUser = async (req, res) => {
     return res.render('edit.ejs', {user: user})
 }
 
+const postEditUser = async (req, res) => {
+    let id = req.body.id;
+    let name = req.body.name;
+    let email = req.body.email;
+    let city = req.body.city;
+    console.log(id, name, email, city)
+
+    // const [results, fields] = await connection.query(
+    //     `INSERT INTO Users (email, name, city) VALUES (?, ?, ?) `,
+    //     [email, name, city],
+    //     function (err, result) {
+    //         if (err) {
+    //             console.log(err.message);
+    //         } else {
+    //             console.log('success'); 
+    //             res.send("success")   
+    //         }
+    //     }
+    // )
+
+    try {
+        const [results, fields] = await connection.query(
+            `UPDATE Users
+            Set email = ?, name = ?, city = ?
+            WHERE id = ?`,
+            [email, name, city, id])
+        console.log(results, fields)
+        console.log('success')
+        res.redirect('/')   
+    } catch (err) {
+        // Log or handle the error here
+        console.error("An error occurred during the query:", err.message);
+        console.error("Error stack trace:", err.stack);
+    }
+}
+
 const getCreateUser = (req, res) => {
     return res.render('create.ejs')
 }
@@ -73,6 +109,7 @@ module.exports = {
     getHomepage,
     getABC,
     getEditUser,
+    postEditUser,
     getCreateUser,
     postCreateUser,
 }
--- Backend_RESTful_nodejs_server/src/routes/web.js
+++ Backend_RESTful_nodejs_server/src/routes/web.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const router = express.Router()
-const {getHomepage, getABC, getEditUser, getCreateUser, postCreateUser} = require('../controllers/homeController')
+const {getHomepage, getABC, getEditUser, postEditUser, getCreateUser, postCreateUser} = require('../controllers/homeController')
 
 // Route 
 // router.Method('/route', handler)
@@ -10,6 +10,8 @@ router.get('/', getHomepage)
 router.get('/abc', getABC)
 
 router.get('/edit/:id', getEditUser)
+router.post('/edit-user', postEditUser)
+
 router.get('/create', getCreateUser)
 router.post('/create-user', postCreateUser)
 
